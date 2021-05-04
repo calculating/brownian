b2457b0850683e63d9562de2d825f05fb6441900
ಠ_ಠ <33814948+calculating@users.noreply.github.com>
2021-05-04 00:17:15 -0400

    Add files via upload
    lol

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ var air = 100
 for (deus_ex_machina = 0; deus_ex_machina < air; deus_ex_machina++) {
     oxygen.push([c.width / 2, c.height / 2, (Math.random() - 0.5) * 10, (Math.random() - 0.5) * 10]);
 }
-var acceleration = 0.05
+var acceleration = 5
 var lightspeed = c.width/10
 var ctx = c.getContext("2d");
-var brownian_motion = setInterval(step, 5);
+var brownian_motion = setInterval(step, 30);
 var sun = [c.width * 0.2, -100]
 
 function step() {
@@ -20,10 +20,22 @@ function step() {
     ctx.fillRect(0, 0, c.width, c.height)
 
     for (molecule = 0; molecule < oxygen.length; molecule++) {
+        // motion blur
+        let previous_X = oxygen[molecule][0]
+        let previous_Y = oxygen[molecule][1]
+
         // bump
         let direction = Math.random() * Math.PI * 2
-        oxygen[molecule][2] += Math.cos(direction) * acceleration * (Math.abs(oxygen[molecule][2]) + lightspeed / 100)
-        oxygen[molecule][3] += Math.sin(direction) * acceleration * (Math.abs(oxygen[molecule][3]) + lightspeed / 100)
+        if (Math.abs(Math.cos(direction)) / Math.cos(direction) == Math.abs(oxygen[molecule][2]) / oxygen[molecule][2]) {
+            oxygen[molecule][2] += Math.cos(direction) * acceleration * (1 - oxygen[molecule][2] / lightspeed) ^ 2
+        } else {
+            oxygen[molecule][2] += Math.cos(direction) * acceleration
+        }
+        if (Math.abs(Math.cos(direction)) / Math.cos(direction) == Math.abs(oxygen[molecule][3]) / oxygen[molecule][3]) {
+            oxygen[molecule][3] += Math.cos(direction) * acceleration * (1 - oxygen[molecule][3] / lightspeed) ^ 2
+        } else {
+            oxygen[molecule][3] += Math.cos(direction) * acceleration
+        }
 
         // avoid breaking the universe
         if (Math.abs(oxygen[molecule][2]) > lightspeed) {
@@ -40,15 +52,19 @@ function step() {
         // border tesseract
         if (oxygen[molecule][0] > c.width) {
             oxygen[molecule][0] -= c.width
+            previous_X = oxygen[molecule][0]
         }
         if (oxygen[molecule][0] < 0) {
             oxygen[molecule][0] += c.width
+            previous_X = oxygen[molecule][0]
         }
         if (oxygen[molecule][1] > c.height) {
             oxygen[molecule][1] -= c.height
+            previous_Y = oxygen[molecule][1]
         }
         if (oxygen[molecule][1] < 0) {
             oxygen[molecule][1] += c.height
+            previous_Y = oxygen[molecule][1]
         }
 
         // draw
@@ -61,6 +77,14 @@ function step() {
         ctx.fill();
 
         ctx.globalAlpha = 1;
+
+        ctx.fillStyle = "#11e91c";
+        ctx.strokeStyle = "#FF0000";
+        ctx.beginPath();
+        ctx.moveTo(oxygen[molecule][0], oxygen[molecule][1])
+        ctx.lineTo(previous_X, previous_Y)
+        ctx.stroke();
+
         ctx.fillStyle = "#843031";
         ctx.beginPath();
         ctx.arc(oxygen[molecule][0], oxygen[molecule][1], 3, 0, 2 * Math.PI);
