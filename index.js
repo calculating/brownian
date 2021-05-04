017c912e399f92dc7621075669aa196e97f98825
ಠ_ಠ <33814948+calculating@users.noreply.github.com>
2021-05-04 13:35:10 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -10,20 +10,20 @@ document.body.onkeyup = function (e) {
 }
 
 var oxygen = []
-var air = 1;
+var air = 10;
 function spontaneous_generation(event) {
     rect = c.getBoundingClientRect();
     cX = event.clientX - rect.left;
     cY = event.clientY - rect.top;
     for (deus_ex_machina = 0; deus_ex_machina < air; deus_ex_machina++) {
-        oxygen.push([cX + Math.random() * 1, cY + Math.random() * 100, Math.random() * 5, Math.random() * 5]);
+        oxygen.push([cX + Math.random() * 1, cY + Math.random() * 1, Math.random(), Math.random()]);
     }
     //step()
 }
 var atomic_radius = 20
 var ctx = c.getContext("2d");
 var brownian_motion = setInterval(step, 30);
-var lightspeed = c.width/100
+var lightspeed = c.width / 100
 
 function step() {
     // clear
@@ -31,65 +31,56 @@ function step() {
     ctx.globalAlpha = 1;
     ctx.fillRect(0, 0, c.width, c.height)
 
-    if (oxygen.length > 1) {
-        for (molecule = 0; molecule < oxygen.length; molecule++) {
-            let bump = [0, 0]
-            for (forces = 0; forces < oxygen.length; forces++) {
-                if (forces !== molecule) {
+    for (molecule = 0; molecule < oxygen.length; molecule++) {
+        let bump = [0, 0]
+        for (forces = 0; forces < oxygen.length; forces++) {
+            if (forces !== molecule) {
 
-                    let distance = Math.sqrt(Math.pow(oxygen[molecule][0] - oxygen[forces][0], 2) + Math.pow(oxygen[molecule][1] - oxygen[forces][1], 2))
-                    let ratioX = (oxygen[molecule][0] - oxygen[forces][0]) / distance
-                    let ratioY = (oxygen[molecule][1] - oxygen[forces][1]) / distance
+                let distance = Math.sqrt(Math.pow(oxygen[molecule][0] - oxygen[forces][0], 2) + Math.pow(oxygen[molecule][1] - oxygen[forces][1], 2))
+                let ratioX = (oxygen[molecule][0] - oxygen[forces][0]) / distance
+                let ratioY = (oxygen[molecule][1] - oxygen[forces][1]) / distance
 
-                    bump[0] += ratioX * (1 / Math.pow(distance, 2))
-                    bump[1] += ratioY * (1 / Math.pow(distance, 2))
-                }
+                bump[0] += ratioX * (1 / Math.pow(distance, 2)) * 10
+                bump[1] += ratioY * (1 / Math.pow(distance, 2)) * 10
             }
+        }
 
-            bump[0] += 5 / oxygen[molecule][0]
-            bump[0] -= 5 / (c.width - oxygen[molecule][0])
+        bump[0] += 5 / oxygen[molecule][0]
+        bump[0] -= 5 / (c.width - oxygen[molecule][0])
 
-            bump[1] += 5 / oxygen[molecule][1]
-            bump[1] -= 5 / (c.height - oxygen[molecule][1])
+        bump[1] += 5 / oxygen[molecule][1]
+        bump[1] -= 5 / (c.height - oxygen[molecule][1])
 
-            let bump_distance = Math.sqrt(Math.pow(bump[0], 2) + Math.pow(bump[1], 2))
-            bump[0] = 5 * (bump[0] / (bump_distance))
-            bump[1] = 5 * (bump[1] / (bump_distance))
+        let bump_distance = Math.sqrt(Math.pow(bump[0], 2) + Math.pow(bump[1], 2))
+        bump[0] = 5 * (bump[0] / (bump_distance))
+        bump[1] = 5 * (bump[1] / (bump_distance))
 
-            ctx.globalAlpha = 1;
+        ctx.globalAlpha = 1;
 
-            ctx.strokeStyle = "#00FF00";
-            ctx.beginPath();
-            ctx.moveTo(oxygen[molecule][0], oxygen[molecule][1])
+        ctx.strokeStyle = "#00FF00";
+        ctx.beginPath();
+        ctx.moveTo(oxygen[molecule][0], oxygen[molecule][1])
 
-            oxygen[molecule][2] += bump[0] * (1 - (oxygen[molecule][2] / lightspeed))
-            oxygen[molecule][3] += bump[1] * (1 - (oxygen[molecule][3] / lightspeed))
-            oxygen[molecule][0] += oxygen[molecule][2]
-            oxygen[molecule][1] += oxygen[molecule][3]
+        oxygen[molecule][2] += bump[0] * (1 - (oxygen[molecule][2] / lightspeed))
+        oxygen[molecule][3] += bump[1] * (1 - (oxygen[molecule][3] / lightspeed))
+        oxygen[molecule][0] += oxygen[molecule][2]
+        oxygen[molecule][1] += oxygen[molecule][3]
 
-            ctx.lineTo(oxygen[molecule][0], oxygen[molecule][1])
-            ctx.stroke();
+        ctx.lineTo(oxygen[molecule][0], oxygen[molecule][1])
+        ctx.stroke();
 
-            // border tesseract
-            if (oxygen[molecule][0] > c.width) {
-                oxygen[molecule][0] = c.width - 10
-            }
-            if (oxygen[molecule][0] < 0) {
-                oxygen[molecule][0] = 10
-            }
-            if (oxygen[molecule][1] > c.height) {
-                oxygen[molecule][1] = c.height - 10
-            }
-            if (oxygen[molecule][1] < 0) {
-                oxygen[molecule][1] = 10
-            }
+        // border tesseract
+        if (oxygen[molecule][0] > c.width || oxygen[molecule][0] < 0 || oxygen[molecule][1] > c.height || oxygen[molecule][1] < 0) {
+            oxygen.splice(molecule, 1)
+        }
 
-            // draw
-            ctx.fillStyle = "#843031";
-            ctx.beginPath();
+        // draw
+        ctx.fillStyle = "#843031";
+        ctx.beginPath();
+        if (oxygen[molecule]) {
             ctx.arc(oxygen[molecule][0], oxygen[molecule][1], 4, 0, 2 * Math.PI);
             ctx.fill();
         }
     }
-    
+
 }
