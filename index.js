d3c1743992276ed538977c6932bfeb019005ecef
ಠ_ಠ <33814948+calculating@users.noreply.github.com>
2021-05-03 23:30:44 -0400

    Add files via upload
    beams

--- index.js
+++ index.js
@@ -3,53 +3,60 @@ var c = document.getElementById("global_warming");
 c.width = dimension[0] - 20;
 c.height = dimension[1] - 25;
 
-var oxygen = [c.width / 2, c.height / 2, (Math.random() - 0.5) * 10, (Math.random() - 0.5) * 10]
+var oxygen = []
+var air = 20
+for (deus_ex_machina = 0; deus_ex_machina < air; deus_ex_machina++) {
+    oxygen.push([c.width / 2, c.height / 2, (Math.random() - 0.5) * 10, (Math.random() - 0.5) * 10]);
+}
 var acceleration = 0.05
 var lightspeed = c.width/10
 var ctx = c.getContext("2d");
 var brownian_motion = setInterval(step, 5);
+var sun = [c.width/2, -10]
 
 function step() {
-    for (frames = 0; frames < 1; frames++) {
+    // clear
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(0, 0, c.width, c.height)
+
+    for (molecule = 0; molecule < oxygen.length; molecule++) {
         // bump
         let direction = Math.random() * Math.PI * 2
-        oxygen[2] += Math.cos(direction) * acceleration * (Math.abs(oxygen[2]) + lightspeed / 100)
-        oxygen[3] += Math.sin(direction) * acceleration * (Math.abs(oxygen[3]) + lightspeed / 100)
+        oxygen[molecule][2] += Math.cos(direction) * acceleration * (Math.abs(oxygen[molecule][2]) + lightspeed / 100)
+        oxygen[molecule][3] += Math.sin(direction) * acceleration * (Math.abs(oxygen[molecule][3]) + lightspeed / 100)
 
         // avoid breaking the universe
-        if (Math.abs(oxygen[2]) > lightspeed) {
-            oxygen[2] = lightspeed * (Math.abs(oxygen[2]) / oxygen[2])
+        if (Math.abs(oxygen[molecule][2]) > lightspeed) {
+            oxygen[molecule][2] = lightspeed * (Math.abs(oxygen[molecule][2]) / oxygen[molecule][2])
         }
-        if (Math.abs(oxygen[3]) > lightspeed) {
-            oxygen[3] = lightspeed * (Math.abs(oxygen[3]) / oxygen[3])
+        if (Math.abs(oxygen[molecule][3]) > lightspeed) {
+            oxygen[molecule][3] = lightspeed * (Math.abs(oxygen[molecule][3]) / oxygen[molecule][3])
         }
 
         // update position
-        oxygen[0] += oxygen[2]
-        oxygen[1] += oxygen[3]
+        oxygen[molecule][0] += oxygen[molecule][2]
+        oxygen[molecule][1] += oxygen[molecule][3]
 
         // border tesseract
-        if (oxygen[0] > c.width) {
-            oxygen[0] -= c.width
+        if (oxygen[molecule][0] > c.width) {
+            oxygen[molecule][0] -= c.width
         }
-        if (oxygen[0] < 0) {
-            oxygen[0] += c.width
+        if (oxygen[molecule][0] < 0) {
+            oxygen[molecule][0] += c.width
         }
-        if (oxygen[1] > c.height) {
-            oxygen[1] -= c.height
+        if (oxygen[molecule][1] > c.height) {
+            oxygen[molecule][1] -= c.height
         }
-        if (oxygen[1] < 0) {
-            oxygen[1] += c.height
+        if (oxygen[molecule][1] < 0) {
+            oxygen[molecule][1] += c.height
         }
 
         // draw
         ctx.fillStyle = "#FFFFFF";
-        ctx.globalAlpha = 0.01;
-        ctx.fillRect(0, 0, c.width, c.height)
-        ctx.fillStyle = "#000000";
-        ctx.globalAlpha = 1;
         ctx.beginPath();
-        ctx.arc(oxygen[0], oxygen[1], 10, 0, 2 * Math.PI);
-        ctx.stroke();
+        ctx.moveTo(oxygen[molecule][0], oxygen[molecule][1]);
+        ctx.lineTo(sun[0] + ((oxygen[molecule][0] - sun[0]) * c.height), sun[1] + ((oxygen[molecule][1] - sun[1]) * c.height));
+        ctx.lineTo(sun[0] + ((oxygen[molecule][0] - sun[0]) * c.height) + c.width, sun[1] + ((oxygen[molecule][1] - sun[1]) * c.height));
+        ctx.fill();
     }
 }
